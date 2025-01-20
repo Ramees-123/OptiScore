0dd2e8eed045124efdb1e1ce0228e842a1eae7b5
Ramees-123 <chukkanrameesramees@gmail.com>
2025-01-20 18:46:59 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,156 +1,105 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:login_page_day_23/login.dart';
+import 'package:login_page_day_23/signup.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ));
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      )
+  );
+}
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var fadeInUp = FadeInUp(
-        duration: Duration(milliseconds: 1300),
-        child: Text(
-          "Welcome Back",
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ));
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-          const Color.fromARGB(255, 92, 4, 33),
-          const Color.fromARGB(255, 69, 4, 64),
-          const Color.fromARGB(255, 213, 55, 213)
-        ])),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  FadeInUp(
-                      duration: Duration(milliseconds: 1000),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      )),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  fadeInUp,
+                  FadeInUp(duration: Duration(milliseconds: 1000), child:Text("Welcome", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30
+                  ),)),
+                  SizedBox(height: 20,),
+                  FadeInUp(duration: Duration(milliseconds: 1200), child: Text("Automatic identity verification which enables you to verify your identity",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 15
+                    ),)),
                 ],
               ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: Container(
+              FadeInUp(duration: Duration(milliseconds: 1400), child: Container(
+                height: MediaQuery.of(context).size.height / 3,
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60))),
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 60,
-                      ),
-                      FadeInUp(
-                          duration: Duration(milliseconds: 1400),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Color.fromRGBO(225, 95, 27, .3),
-                                      blurRadius: 20,
-                                      offset: Offset(0, 10))
-                                ]),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Colors.grey.shade200))),
-                                  child: TextField(
-                                    decoration: InputDecoration(
-                                        hintText: "Email or Phone number",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey),
-                                        border: InputBorder.none),
-                                  ),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Colors.grey.shade200))),
-                                  child: TextField(
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                        hintText: "Password",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey),
-                                        border: InputBorder.none),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      FadeInUp(
-                          duration: Duration(milliseconds: 1500),
-                          child: Text(
-                            "Forgot Password?",
-                            style: TextStyle(color: Colors.grey),
-                          )),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      FadeInUp(
-                          duration: Duration(milliseconds: 1600),
-                          child: MaterialButton(
-                            onPressed: () {},
-                            height: 50,
-                            // margin: EdgeInsets.symmetric(horizontal: 50),
-                            color: const Color.fromARGB(255, 146, 13, 67),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            // decoration: BoxDecoration(
-                            // ),
-                            child: Center(
-                              child: Text(
-                                "Login",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          )),
-                      SizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  ),
+                    image: DecorationImage(
+                        image: AssetImage('assets/illustration.png')
+                    )
                 ),
-              ),
-            )
-          ],
+              )),
+              Column(
+                children: <Widget>[
+                  FadeInUp(duration: Duration(milliseconds: 1500), child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text("Login", style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18
+                    ),),
+                  )),
+                  SizedBox(height: 20,),
+                  FadeInUp(duration: Duration(milliseconds: 1600), child: Container(
+                    padding: EdgeInsets.only(top: 3, left: 3),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                        )
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                      },
+                      color: Colors.yellow,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)
+                      ),
+                      child: Text("Sign up", style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),),
+                    ),
+                  ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
